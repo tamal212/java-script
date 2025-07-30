508966e7d116caaed870b4a04be8c2391648535c
garu <tamalroy74658@gmail.com>
2025-07-30 21:16:24 +0600

    fumction

---
+++ one.js
@@ -0,0 +1,15 @@
+console.log('sum');
+let sum = 19 + 1;
+console.log(sum);
+if (sum > 19) { console.log('costly') } else { console.log('cheap') } 
+
+let flower ='beli';
+console.log(`${flower} is the best all flower. ${flower} is my favourite`);
+console.log('hello');
+//var number = 10;
+//console.log(number);
+/*let input = prompt('give a number');
+let number = parseInt(input);
+if(number%2===0){console.log(`${number} is a even number`);}else{console.log(`${number} is a odd number`);}*/
+function show() { console.log('like'); }
+document.getElementById('ok').addEventListener('click', show);
