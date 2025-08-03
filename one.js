7a0ae504d74cef24f9c1240e562cd2e7fe2f9092
garu <tamalroy74658@gmail.com>
2025-08-03 09:24:17 +0600

    js event

--- one.js
+++ one.js
@@ -1,15 +1,53 @@
-console.log('sum');
+/*console.log('sum');
 let sum = 19 + 1;
 console.log(sum);
 if (sum > 19) { console.log('costly') } else { console.log('cheap') } 
-
 let flower ='beli';
 console.log(`${flower} is the best all flower. ${flower} is my favourite`);
-console.log('hello');
-//var number = 10;
-//console.log(number);
+console.log('hello');*/
+
 /*let input = prompt('give a number');
 let number = parseInt(input);
-if(number%2===0){console.log(`${number} is a even number`);}else{console.log(`${number} is a odd number`);}*/
+if(number%2===0){console.log(`${number} is a even number`);}else{console.log(`${number} is a odd number`);}
 function show() { console.log('like'); }
-document.getElementById('ok').addEventListener('click', show);
+document.getElementById('ok').addEventListener('click', show);  
+//const neW= Symbol('one');
+const data = {
+  name: 'anu',
+  age: 12,
+  //[neW]: 'number one',
+  moreAbout: function () { return `I am ${this.name}.I am ${this.age}`; },
+};
+console.log(data.moreAbout());
+/*console.log(data.hasOwnProperty('name'));
+console.log(Object.keys(data));
+console.log(data[neW]);
+function sayHello(name) {
+  console.log('hello  ' + name);
+}
+sayHello('lili');*/
+const number = ['mango', 'apple', 'orange'];
+number[3] = 'banana'; 
+number[4] = 'guava';
+console.log(number[3]);
+//number.pop();
+console.log(number);
+//add first item
+number.unshift('milk');
+//remive first item
+number.shift();
+console.log(number.toString());
+console.log(number[number.length - 1]);
+console.log(number.join(', '));
+//different different array add in one
+const x=[1,3,4]
+const y = [1, 36, 4]
+const z = x.concat(y);
+console.log(z.toString());
+
+/*const man = [];
+man['firstName']='apu';
+man['laststName'] = 'avi';
+console.log(man.length);
+console.log(man);*/
+
